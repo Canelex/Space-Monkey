984bbd7894cda8d90e79d0ffeed0ff2c3abecb24
canelex <timkozlov@gmail.com>
2018-07-28 13:41:33 -0700

    Added line renderer connecting levels in level select

---
+++ Assets/Scripts/LevelSelectLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class LevelSelectLine : MonoBehaviour
+{
+    private LineRenderer line;
+
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+
+        LevelSelectItem[] items = FindObjectsOfType<LevelSelectItem>();
+        Array.Sort(items, CompareItems);
+
+        line.positionCount = items.Length;
+        for (int i = 0; i < items.Length; i++)
+        {
+            line.SetPosition(i, (Vector2)items[i].transform.parent.position);
+        }
+    }
+
+    private int CompareItems(LevelSelectItem a, LevelSelectItem b)
+    {
+        if (a.levelIndex > b.levelIndex) return 1;
+        if (a.levelIndex < b.levelIndex) return -1;
+        return 0;
+    }
+}
